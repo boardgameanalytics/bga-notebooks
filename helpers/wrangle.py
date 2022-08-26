b48598505082212d04be212219aca2839b0ebfa0
Randy Nance <randynobx@gmail.com>
2022-08-26 17:37:16 -0400

    added wrangle() to wrangle.py

--- helpers/wrangle.py
+++ helpers/wrangle.py
@@ -3,7 +3,27 @@ import pandas as pd
 from category_encoders import OneHotEncoder
 
 
-# Helper function for encoding game classifications
+def wrangle(df):
+    """Clean dataset and add some composite features"""
+    release_yr_threshold = 1950
+    next_year = 2023
+    # Drop all releases prior to threshold, and any with a future release_year
+    df = df.loc[(df.release_year > release_yr_threshold) &
+                (df.release_year < next_year)]
+    
+    # Set booleans as int
+    df.kickstarter = df.kickstarter.astype(int)
+    
+    # Copies per year (since release) metric
+    # Calculate age of game as 'next year' minus release year, so that games
+    # just released in the current calendar year are counted as being 1 year
+    # old and avoiding division by 0
+    df['copies_per_year'] = (df.owned_copies /
+                             (next_year - df.release_year)).astype(int)
+
+    return df
+
+
 def encode_class(df: pd.DataFrame, name: str) -> pd.DataFrame:
     """Encode game classifications with OHE and groupby game
 
@@ -18,24 +38,8 @@ def encode_class(df: pd.DataFrame, name: str) -> pd.DataFrame:
     return ohe.fit_transform(df).groupby(df.index.values).agg(max)
 
 
-def clean_data(df: pd.DataFrame) -> pd.DataFrame:
-    """Clean dataframe
-
-    Args:
-        df (DataFrame): df to clean
-
-    Returns:
-        Cleaned dataframe
-    """
-    # Cutoff releases before 1970
-    df = df.loc[df['release_year'] > 1950]
-    
-    df.kickstarter = df.kickstarter.astype(int)
-
-    return df
-
-def make_generic(mechanic: str) -> str:
-    '''Condense mechanics to fewer types'''
+def reduce_mechanics(mechanic: str) -> str:
+    """Combine similiar mechanics to reduce total number of mechanic types"""
     if 'action' in mechanic:
         return 'action'
     elif 'auction' in mechanic:
--- tests/test_wrangle.py
+++ tests/test_wrangle.py
@@ -17,21 +17,3 @@ def test_encode_class() -> None:
     out_df = encode_class(in_df, 'mechanic')
     assert isinstance(out_df, pd.DataFrame), 'Did not return a pandas DataFrame'
     assert out_df.shape == (3, 4), 'Incorrectly shaped DataFrame returned'
-
-
-def test_clean_data() -> None:
-    """Test for clean_data()
-    Input:
-        DataFrame with (int) 'release_year' column
-    Expected output:
-        Given DataFrame with all release_year's prior to 1951 removed.
-    """
-    in_df = pd.DataFrame({
-        'id': [0, 1, 2, 3, 4, 5, 6],
-        'title': ['title1', 'title2', 'title3', 'title4', 'unknown', 'title6', 'ancient'],
-        'release_year': [2020, 2002, 1969, 1950, 0, 1458, -2300]
-    })
-    out_df = clean_data(in_df)
-    assert isinstance(out_df, pd.DataFrame), 'Did not return a pandas DataFrame'
-    assert out_df.shape == (3, 3), 'Incorrectly shaped DataFrame returned'
-    assert (out_df['release_year'] <= 1950).sum() == 0, 'Returned pre-1951 game'
