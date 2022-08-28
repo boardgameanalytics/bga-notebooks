efb29a4160992aafc30be46d58e44a51a790af75
Randy Nance <randynobx@gmail.com>
2022-08-28 10:18:59 -0400

    added measuring popularity notebook

--- helpers/wrangle.py
+++ helpers/wrangle.py
@@ -4,20 +4,25 @@ import pandas as pd
 from category_encoders import OneHotEncoder
 
 
-def wrangle(df):
+def clean_data(df, threshold: int=None):
     """Clean dataset and add some composite features"""
-    release_yr_threshold = 1950
     next_year = 2023
     # Drop all releases prior to threshold, and any with a future release_year
-    df = df.loc[(df.release_year > release_yr_threshold) &
-                (df.release_year < next_year)]
+    if threshold:
+        df = df.loc[df.release_year > threshold]
+    df = df.loc[df.release_year < next_year]
     
+    # Drop NA equivalent values for release year and weight
+    df = df.loc[df.release_year != 0]
+    df = df.loc[df.weight != 0]
+    
+    # Remove extreme outliers
+    df = df.loc[df.max_players < 200]
+    df = df.loc[df.max_playtime < 1000]
+
     # Set booleans as int
     df.kickstarter = df.kickstarter.astype(int)
     
-    # Feature engineering
-    df = add_composite_features(df)
-
     return df
 
 
@@ -47,18 +52,17 @@ def add_composite_features(df: pd.DataFrame) -> pd.DataFrame:
     # metric for how positively or negatively the game is generally viewed!
     df['popularity'] = ((df.bayes_rating - 5.5) * df.total_ratings)
 
-    # Adjust the popularity metric to a smaller scale using the natural log
     def adjust_pop(n):
-        # Keep scale centered at zero, return 0 before taking log
-        if n == 0:
-            return 0
-        # Multiply the absolute value to move all values above 1 to
-        # keep results positive
-        ap = np.log(np.abs(n) * 100)
+        """Adjust the popularity metric to a smaller scale using the natural log"""
+        # Add 1 to the absolute value to keep all resulting numbers positive,
+        # with a lower bound of 0
+        ap = np.log(np.abs(n) + 1)
+        
         # Keep the sign of the unadjusted number
         if n < 0:
             ap *= -1
         return ap
+
     # Save the adjusted metric as a separate column
     df['popularity_adj'] = df.popularity.apply(adjust_pop)
 
